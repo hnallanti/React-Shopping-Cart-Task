d6d066e346fd5d338e90d2de29dade4153ee5de9
Kalyankumar <kalyankumarpulapaka@gmail.com>
2024-05-06 00:04:23 +0530

    Initial commit

---
+++ React_pre1/src/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./css/styles.css"; // Make sure your CSS file path is correct here
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
